adc385410848b2201336c590427691e2dfa925ce
julianboyne <julianboyne11@gmail.com>
2022-10-02 20:00:20 -0400

    implement game-owner-profile view functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,13 +1,17 @@
 import { Profile } from "../models/profile.js"
 
 function showMyProfile(req, res) {
-  Profile.findById(req.params.id)
-  .then(profile => {
-    const isSelf = profile._id.equals(req.user.profile._id)
-    res.render('profiles/show', {
-      title: `${profile.name}'s profile`,
-      isSelf,
-      profile,
+  Profile.find({})
+  .then(profiles => {
+    Profile.findById(req.params.id)
+    .then(profile => {
+      const isSelf = profile._id.equals(req.user.profile._id)
+      res.render('profiles/show', {
+        title: `${profile.name}'s profile`,
+        isSelf,
+        profiles,
+        profile,
+      })
     })
   })
 }
